78a3e2a63ad40f9bf5074918b0e861c7bba13da3
lofiey <42893670+lofiey@users.noreply.github.com>
2025-12-22 13:59:29 +0800

    Update server.go

--- internal/dnsserver/server.go
+++ internal/dnsserver/server.go
@@ -8,8 +8,9 @@ import (
 )
 
 type Server struct {
-	addr   string
-	server *dns.Server
+	addr      string
+	udpServer *dns.Server
+	tcpServer *dns.Server
 }
 
 func New(addr string) *Server {
@@ -20,7 +21,7 @@ func (s *Server) Start() error {
 	handler := dns.NewServeMux()
 	handler.HandleFunc(".", s.handleQuery)
 
-	s.server = &dns.Server{
+	s.udpServer = &dns.Server{
 		Addr:         s.addr,
 		Net:          "udp",
 		Handler:      handler,
@@ -28,18 +29,35 @@ func (s *Server) Start() error {
 		WriteTimeout: 5 * time.Second,
 	}
 
-	return s.server.ListenAndServe()
+	s.tcpServer = &dns.Server{
+		Addr:         s.addr,
+		Net:          "tcp",
+		Handler:      handler,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		if err := s.tcpServer.ListenAndServe(); err != nil {
+			log.Println("tcp dns failed:", err)
+		}
+	}()
+
+	return s.udpServer.ListenAndServe()
 }
 
 func (s *Server) Stop() {
-	if s.server != nil {
-		_ = s.server.Shutdown()
+	if s.udpServer != nil {
+		_ = s.udpServer.Shutdown()
+	}
+	if s.tcpServer != nil {
+		_ = s.tcpServer.Shutdown()
 	}
 }
 
 func (s *Server) handleQuery(w dns.ResponseWriter, r *dns.Msg) {
 	c := new(dns.Client)
-	c.Net = "udp"
+	c.Net = w.RemoteAddr().Network()
 
 	resp, _, err := c.Exchange(r, "8.8.8.8:53")
 	if err != nil {
