06153e58a1c8eb94211f5a79efc2baca17550642
lofiey <42893670+lofiey@users.noreply.github.com>
2025-12-22 13:57:12 +0800

    Update main.go

--- cmd/dns-core/main.go/main.go
+++ cmd/dns-core/main.go/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"dns-core/internal/dnsserver"
+)
+
 func main() {
-    // Entry point
+	srv := dnsserver.New(":53")
+
+	go func() {
+		if err := srv.Start(); err != nil {
+			log.Fatalf("dns start failed: %v", err)
+		}
+	}()
+
+	log.Println("DNS server started on :53")
+
+	// 等待退出信号
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+
+	log.Println("shutting down DNS server")
+	srv.Stop()
 }
