ab2111e509bcd6307145a74b041685d4ab245951
lofiey <42893670+lofiey@users.noreply.github.com>
2025-12-22 14:02:53 +0800

    Update response.go

--- internal/blocker/response.go
+++ internal/blocker/response.go
@@ -1,1 +1,17 @@
-// blocked dns response
+package blocker
+
+import "github.com/miekg/dns"
+
+func BlockResponse(req *dns.Msg) *dns.Msg {
+	m := new(dns.Msg)
+	m.SetReply(req)
+
+	for _, q := range req.Question {
+		if q.Qtype == dns.TypeA {
+			rr, _ := dns.NewRR(q.Name + " 60 IN A 0.0.0.0")
+			m.Answer = append(m.Answer, rr)
+		}
+	}
+
+	return m
+}
