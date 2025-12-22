66b7fce674883290e929a07ca86ba0d7e747e409
lofiey <42893670+lofiey@users.noreply.github.com>
2025-12-23 01:45:10 +0800

    Update app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,23 @@
-// app lifecycle
+package app
+
+import (
+	"log"
+
+	"fpqdns/internal/config"
+	"fpqdns/internal/dnsserver"
+)
+
+func Run() error {
+	cfg, err := config.Load("config.yaml")
+	if err != nil {
+		return err
+	}
+
+	srv := dnsserver.New(cfg.Listen.DNS)
+	if err := srv.Start(); err != nil {
+		return err
+	}
+
+	log.Println("[app] fpqdns started")
+	select {}
+}
