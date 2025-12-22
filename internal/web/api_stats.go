a0fecc14eb0da23fe17270e4bf3a66a355281a04
lofiey <42893670+lofiey@users.noreply.github.com>
2025-12-22 23:48:17 +0800

    Create api_stats.go

---
+++ internal/web/api_stats.go
@@ -0,0 +1,37 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+	"runtime"
+	"time"
+
+	"dns-core/internal/metrics"
+)
+
+func statsHandler(w http.ResponseWriter, _ *http.Request) {
+	m := metrics.Global
+
+	resp := map[string]any{
+		"uptime": time.Since(m.StartTime).String(),
+		"query": map[string]uint64{
+			"total":   m.Query.Total,
+			"cn":      m.Query.CN,
+			"foreign": m.Query.Foreign,
+		},
+		"cache": map[string]uint64{
+			"hit":  m.CacheHit,
+			"miss": m.CacheMiss,
+		},
+		"top": map[string]any{
+			"domain": m.TopDomain.Top(5),
+			"client": m.TopClient.Top(5),
+		},
+		"runtime": map[string]any{
+			"goroutine": runtime.NumGoroutine(),
+			"memory":    runtime.MemStats{},
+		},
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
